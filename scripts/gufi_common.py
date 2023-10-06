dd51f0104e6d9a04313aceef8d9d72f69aa35809
John Bent <johnbent@gmail.com>
2023-10-05 19:59:01 -0600

    fixed tab issues found by pylint

--- scripts/gufi_common.py
+++ scripts/gufi_common.py
@@ -259,13 +259,13 @@ def build_query(select, tables, where=None, group_by=None,
 
 # a helper function to show the gufi_query being executed. Helpful for debugging and education.
 def print_query(query_tokens):
-  formatted_string = ""
-  for index, token in enumerate(query_tokens):
-      if token.startswith('-') or index == len(query_tokens)-1:
-          formatted_string += '\n    ' + token
-      else:
-          formatted_string += ' ' + token
-  print('GUFI query is \n  {0}'.format(formatted_string))
+    formatted_string = ""
+    for index, token in enumerate(query_tokens):
+        if token.startswith('-') or index == len(query_tokens)-1:
+            formatted_string += '\n    ' + token
+        else:
+            formatted_string += ' ' + token
+    print('GUFI query is \n  {0}'.format(formatted_string))
 
 def add_common_flags(parser):
     '''Common GUFI tool flags'''
